a4d3b4a7414962e0964602d3218827ef91d65103
roy-abir05 <royabirdhn@gmail.com>
2025-01-06 18:41:19 +0530

    Sidebar added

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import SideLinks from "./SideLinks"
+
+const Sidebar = () => {
+    return (
+        <div className="w-[18vw] bg-[#22252e] overflow-hidden">
+            <div className="flex items-center justify-between mb-10 xl:p-8 p-6">
+                <img src="/404 Found Logo.jpeg" alt="Logo" className="w-16 h-16 rounded-lg"/>
+                <span className="font-mono -tracking-widest font-light text-[40px] text-white">Insightly</span>
+            </div>
+
+            <SideLinks />
+
+
+        </div>
+    )
+}
+
+export default Sidebar
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
+import Sidebar from "./components/Sidebar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Router>
-    <App />
+    <div className="w-screen flex">
+      <Sidebar className="h-full w-fit top-0 left-0" />
+      <App />
+    </div>
   </Router>
 );
