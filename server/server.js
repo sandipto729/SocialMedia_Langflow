99c2c205da44b20984d65ad15d2ff85d0464180b
sandipto729 <sandipto729@gmail.com>
2025-01-07 01:08:50 +0530

    Refactor Vite configuration and update CORS origin to use environment variable

--- server/server.js
+++ server/server.js
@@ -12,14 +12,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: process.env.Frontend_URL,
         methods: ['GET', 'POST'],
         credentials: true,
     },
 });
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.Frontend_URL, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
